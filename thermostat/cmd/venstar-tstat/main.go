235150f09f5fad240ad96d985106367d5fb898ab
Mike Mason <mike-git@ca.mrm.land>
2020-09-19 09:19:26 -0500

    add terrible runtime table printer

--- thermostat/cmd/venstar-tstat/main.go
+++ thermostat/cmd/venstar-tstat/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/mikemrm/go-venstar/thermostat"
 )
@@ -67,17 +69,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	for i, runtime := range runtimes {
-		if i == 0 {
-			fmt.Println("  +-----------------+--------+--------+--------+--------+-------+-------+----+----+")
-			fmt.Println("  | Timestamp       | Heat 1 | Heat 2 | Cool 1 | Cool 2 | Aux 1 | Aux 2 | FC | OV |")
-			fmt.Println("  +-----------------+--------+--------+--------+--------+-------+-------+----+----+")
-		}
-		fmt.Printf("  | %15d | %6d | %6d | %6d | %6d | %5d | %5d | %2d | %2d |\n", runtime.Time, runtime.Heat1, runtime.Heat2, runtime.Cool1, runtime.Cool2, runtime.Aux1, runtime.Aux2, runtime.FC, runtime.OV)
-		if i == len(runtimes)-1 {
-			fmt.Println("  +-----------------+--------+--------+--------+--------+-------+-------+----+----+")
-		}
-	}
+	printRuntimes(runtimes)
 
 	fmt.Println("Query Alerts:")
 	alerts, err := t.GetQueryAlerts()
@@ -87,4 +79,81 @@ func main() {
 	for _, alert := range alerts {
 		fmt.Printf("  %10s: Active = %v\n", alert.Name, alert.Active)
 	}
+}
+
+func printRuntimes(runtimes []*thermostat.Runtime) {
+	tsFormat := "2006-01-02 15:04:05 MST"
+	colWidths := make(map[string]int)
+	rowValues := make([]map[string]string, len(runtimes))
+	for i, runtime := range runtimes {
+		values := make(map[string]string)
+		values["Timestamp"] = runtime.Timestamp.Format(tsFormat)
+		values["Free Cooling"] = strconv.Itoa(runtime.FreeCooling)
+		values["Override"] = strconv.Itoa(runtime.Override)
+		for k, v := range runtime.Heaters {
+			idx := "Heat " + k
+			values[idx] = strconv.Itoa(v)
+		}
+		for k, v := range runtime.Coolers {
+			idx := "Cool " + k
+			values[idx] = strconv.Itoa(v)
+		}
+		for k, v := range runtime.Aux {
+			idx := "Aux " + k
+			values[idx] = strconv.Itoa(v)
+		}
+		for k, v := range values {
+			vLen := len(v)
+			if l, ok := colWidths[k]; ok {
+				if vLen > l {
+					colWidths[k] = vLen
+				}
+			} else {
+				colWidths[k] = len(k)
+			}
+		}
+		rowValues[i] = values
+	}
+	colOrder := make([]string, len(colWidths))
+	colOrder[0] = "Timestamp"
+	colOrder[len(colWidths)-2] = "Free Cooling"
+	colOrder[len(colWidths)-1] = "Override"
+	colNext := 1
+	for k := range colWidths {
+		if strings.HasPrefix(k, "Heat") {
+			colOrder[colNext] = k
+			colNext++
+		}
+	}
+	for k := range colWidths {
+		if strings.HasPrefix(k, "Cool") {
+			colOrder[colNext] = k
+			colNext++
+		}
+	}
+	for k := range colWidths {
+		if strings.HasPrefix(k, "Aux") {
+			colOrder[colNext] = k
+			colNext++
+		}
+	}
+	divBits := make([]string, len(colWidths))
+	values := make([]string, len(colWidths))
+	for i, k := range colOrder {
+		width := colWidths[k]
+		values[i] = fmt.Sprintf("%-*s", width, k)
+		divBits[i] = strings.Repeat("-", width)
+	}
+	divider := fmt.Sprintf("  +-%s-+", strings.Join(divBits, "-+-"))
+	fmt.Println(divider)
+	fmt.Printf("  | %s |\n", strings.Join(values, " | "))
+	fmt.Println(divider)
+	for _, row := range rowValues {
+		for i, k := range colOrder {
+			width := colWidths[k]
+			values[i] = fmt.Sprintf("%*s", width, row[k])
+		}
+		fmt.Printf("  | %s |\n", strings.Join(values, " | "))
+	}
+	fmt.Println(divider)
 }
